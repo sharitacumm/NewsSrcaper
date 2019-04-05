46e06543db605014dfab34b81d4f0e15811411ba
sharitacumm <emandraine@yahoo.com>
2019-04-04 20:01:43 -0400

    heroku create app

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var articleModel = require("./models/article.js");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
